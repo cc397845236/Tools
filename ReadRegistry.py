85b6f9fa10bedfdbcdf3830ce23f843d74a2bcdb
wangchao112872 <wangchao112872@pwrd.com>
2018-05-08 18:39:31 +0800

    add Read Registry Scripts

---
+++ ReadRegistry.py
@@ -0,0 +1,16 @@
+#coding:utf-8
+
+import _winreg
+
+def ReadRegistryValue(KeyName, ValueName):
+    try:
+        openKey = _winreg.OpenKey(_winreg.HKEY_CURRENT_USER, KeyName)
+        path, type = _winreg.QueryValueEx(openKey, ValueName)
+        print path, type
+        return path
+    except WindowsError, ex:
+        print "Read RegistryValue Failed " ,ex
+        return 0
+
+if __name__ == "__main__":
+    ReadRegistryValue(r"Software\Valve1\Steam", "SteamPath")
