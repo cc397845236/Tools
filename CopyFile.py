626f361f995db80669e3b0323c5eaebd21672b57
wangchao112872 <wangchao112872@pwrd.com>
2018-05-09 17:24:58 +0800

    add Read Registry Scripts

--- CopyFile.py
+++ CopyFile.py
@@ -22,10 +22,10 @@ def CopyFilesFunc(srcdir, dstdir):
             elif os.path.isdir(srcFile):
                 copyDir(srcFile,dstFile)
             else:
-                print "Path error, please check!"
+                print ("Path error, please check!")
                 return False
     else:
-        print "Path error, please check!"
+        print ("Path error, please check!")
         return False
 
 def copyFile(srcFile, dstFile):
@@ -33,26 +33,26 @@ def copyFile(srcFile, dstFile):
         shutil.copyfile(srcFile, dstFile)
     # 若文件存在，判断目的文件与源文件修改时间是否一致，若不同删除目的文件
     elif os.path.exists(dstFile) and (os.path.getmtime(dstFile) != os.path.getmtime(srcFile)):
-        print srcFile + " was not latest!"
+        print (srcFile + " was not latest!")
         os.remove(dstFile)
-        print srcFile + " removed successfully!"
+        print (srcFile + " removed successfully!")
         shutil.copyfile(srcFile, dstFile)
     else:
-        print "File already existed and was latest!"
-    print srcFile + " copy completed!"
+        print ("File already existed and was latest!")
+    print (srcFile + " copy completed!")
 
 def copyDir(srcDir, dstDir):
     if not os.path.exists(dstDir):
         shutil.copytree(srcDir, dstDir)
     # 若文件夹存在，判断目的文件与源文件修改时间是否一致，若不同删除目的文件
     elif os.path.exists(dstDir) and (os.path.getmtime(dstDir) != os.path.getmtime(srcDir)):
-        print srcDir + " was not latest!"
+        print (srcDir + " was not latest!")
         shutil.rmtree(dstDir)
-        print srcDir + " removed successfully!"
+        print (srcDir + " removed successfully!")
         shutil.copytree(srcDir, dstDir)
     else:
-        print "Fold already existed and was latest!"
-    print srcDir + " copy completed!"
+        print ("Fold already existed and was latest!")
+    print (srcDir + " copy completed!")
 
 if __name__ == '__main__':
     CopyFilesFunc(sys.argv[1], sys.argv[2])
--- ReadRegistry.py
+++ ReadRegistry.py
@@ -1,15 +1,15 @@
 #coding:utf-8
 
-import _winreg
+import winreg
 
 def ReadRegistryValue(KeyName, ValueName):
     try:
-        openKey = _winreg.OpenKey(_winreg.HKEY_CURRENT_USER, KeyName)
-        path, type = _winreg.QueryValueEx(openKey, ValueName)
+        openKey = winreg.OpenKey(winreg.HKEY_CURRENT_USER, KeyName)
+        path, type = winreg.QueryValueEx(openKey, ValueName)
         # print path, type
         return path
-    except WindowsError, ex:
-        print "Read RegistryValue Failed " ,ex
+    except WindowsError as ex:
+        print ("Read RegistryValue Failed ", ex )
         return 0
 
 if __name__ == "__main__":
